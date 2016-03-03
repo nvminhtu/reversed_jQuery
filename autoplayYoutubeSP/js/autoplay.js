a3de538c3afffc68e39e332c39996046bda87015
Tu Nguyen <nvminhtu@gmail.com>
2016-03-03 08:57:23 +0700

    autoplay Youtube,  work with Manific Popup, jQuery navigate in mobile (back & forward)

---
+++ autoplayYoutubeSP/js/autoplay.js
@@ -0,0 +1,20 @@
+;(function($) {
+  
+  "use strict";
+  
+  $(function(){
+    // $('.thumb_video').css({
+    //   display: 'none'
+    // });
+    $('.introVid a').off().click(function(){
+      event.preventDefault();
+      autoPlayVideo('o6NstKZMr94','450','283');
+      $(this).find('.thumb_video').fadeOut('400');
+      $(this).find('#videoContainer').fadeIn('400');
+    });
+  });
+ function autoPlayVideo(vcode, width, height){
+  $("#videoContainer").html('<iframe width="'+width+'" height="'+height+'" src="https://www.youtube.com/embed/'+vcode+'?autoplay=1&loop=1&rel=0&wmode=transparent" frameborder="0" allowfullscreen wmode="Opaque"></iframe>');
+  }
+
+})(jQuery);
